261f8389d44cd6e3432d9be7381e10826beb2414
Andrew Tran <atran@nextdoor.com>
2015-07-17 15:35:44 -0700

    Reflects server API changes

--- app/src/main/java/com/nextdoor/stfandroid/STFConfig.java
+++ app/src/main/java/com/nextdoor/stfandroid/STFConfig.java
@@ -3,7 +3,7 @@ package com.nextdoor.stfandroid;
 import android.os.Environment;
 
 public class STFConfig {
-    public static final String API_SERVER = "http://stf-web-01.herokuapp.com/bug/";
+    public static final String API_SERVER = "http://stf-web-02.herokuapp.com/bug/";
     public static final String DEFAULT_EMAIL = "";
     public static final String JIRA_PROJECT = "STF1";
     public static final String APP_COLOR = "#1E9E5E";
--- app/src/main/java/com/nextdoor/stfandroid/STFJira.java
+++ app/src/main/java/com/nextdoor/stfandroid/STFJira.java
@@ -11,19 +11,24 @@ public class STFJira {
     private static final String JIRA_ISSUETYPE = "issuetype";
     private static final String JIRA_ISSUETYPE_NAME = "name";
     private static final String JIRA_ATTACHMENT = "image";
+    private static final String JIRA_REPORTER = "reporter";
+    private static final String JIRA_ISSUE = "issue";
     private static final String TYPE = "Bug";
 
     public static JSONObject getRequest(String email, String summary, String encodedImage) {
         JSONObject requestJson = new JSONObject();
+        JSONObject issueJson = new JSONObject();
         JSONObject projectJson = new JSONObject();
         JSONObject issueTypeJson = new JSONObject();
         try {
             projectJson.put(JIRA_PROJECT_KEY, STFConfig.JIRA_PROJECT);
             issueTypeJson.put(JIRA_ISSUETYPE_NAME, TYPE);
-            requestJson.put(JIRA_PROJECT, projectJson);
-            requestJson.put(JIRA_SUMMARY, summary);
-            requestJson.put(JIRA_DESCRIPTION, "");
-            requestJson.put(JIRA_ISSUETYPE, issueTypeJson);
+            issueJson.put(JIRA_PROJECT, projectJson);
+            issueJson.put(JIRA_SUMMARY, summary);
+            issueJson.put(JIRA_DESCRIPTION, "");
+            issueJson.put(JIRA_ISSUETYPE, issueTypeJson);
+            requestJson.put(JIRA_ISSUE, issueJson);
+            requestJson.put(JIRA_REPORTER, email);
             requestJson.put(JIRA_ATTACHMENT, encodedImage);
         } catch (JSONException e) {
         }
